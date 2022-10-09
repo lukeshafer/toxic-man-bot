58ab72089d9e3664c66871870bef4f9210d4ad98
Luke Shafer <lukealanshafer@gmail.com>
2022-10-09 19:00:22 -0400

    Add request varification and ping response

--- src/pages/api/bot/index.ts
+++ src/pages/api/bot/index.ts
@@ -5,14 +5,40 @@ import {
   verifyKey,
 } from "discord-interactions";
 
-interface ResponseData {
-  name: string;
-}
+// interface ResponseData {
+//   name: string;
+// }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.status(200).json({ name: "John Doe" });
+
+  if (req.method === "POST") {
+    const signature = req.headers["x-signature-ed25519"];
+    const timestamp = req.headers["x-signature-timestamp"];
+    const rawBody = JSON.stringify(req.body);
+
+    const isValidRequest = verifyKey(
+      rawBody,
+      signature,
+      timestamp,
+      process.env.PUBLIC_KEY
+    );
+
+    if (!isValidRequest) {
+      console.error("Invalid Request");
+      return res.status(401).send({ error: "Bad request signature " });
+    }
+
+    const message = req.body;
+
+    if (message.type === InteractionType.PING) {
+      console.log("Handling Ping request");
+      res.send({
+        type: InteractionResponseType.PONG,
+      });
+    } else {
+      console.error("Unknown Type");
+      res.status(400).send({ error: "Unknown Type" });
+    }
+  }
 }
